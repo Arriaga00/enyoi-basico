e7d354c2c8603a2867779f99b9b6968d67a1b308
andres arriaga <andresfelipearriaga@gmail.com>
2024-02-21 22:07:45 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const d = document
+const $email = d.getElementById('correo')
+const $password = d.getElementById('password')
+const $btnIngreso = d.getElementById('ingresar')
+
+const usuarios = [
+    {
+        nombre : "Diego Jhondoe",
+        email: "diego@gmail.com",
+        contraseña: "Jhondoe"
+      },
+      {
+        nombre : "Jane Smith",
+        email: "Jane.Smith@gmail.com",
+        contraseña: "S3cur3P@ss"
+      }
+]
+
+$btnIngreso.addEventListener('click',()=>{
+    const email = $email.value
+    const password = $password.value
+    usuarios.map(el=>{
+        if(el.email === email && el.contraseña=== password) {
+            window.open('../pages/formulario.html')
+        }else{
+             console.warn(`ereror`)
+            }
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+   ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
